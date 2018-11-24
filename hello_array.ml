8240e9ad591c0fa2d23ebdce22374a6e73b82e56
albertgoncalves <goncalves.albert@gmail.com>
2018-11-24 17:38:02 -0500

    Better setup.

--- hello_array.ml
+++ hello_array.ml
@@ -1,14 +1,18 @@
 let dim = 8
+
 let arr =
     let arr = Array.make dim 0 in
     Array.set arr 6 1;
     arr.(2) <- 8;
     arr
+
 let mat =
     let mat = Array.make_matrix (dim / 2) dim 0 in
     mat.(1).(1) <- 1;
     mat
 
-let () =
+let main =
     Printf.printf "%d\n" @@ Array.length arr;
     Array.iter (fun x -> print_int @@ Array.length x) mat; print_newline ()
+
+let () = main
