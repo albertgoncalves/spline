b13731b7c0e16d37edc5d6f0f6eb1e613fc41fbd
albertgoncalves <goncalves.albert@gmail.com>
2018-11-24 19:45:58 -0500

    Float range.

--- hello_array.ml
+++ hello_array.ml
@@ -14,9 +14,25 @@ let mat =
     mat.(x).(y) <- 9;
     mat
 
+let frange a b incr =
+    let dist = b -. a in
+    let check, incr =
+        if b > a then
+            (<=), incr *. (-1.0)
+        else
+            (>=), incr in
+    let rec loop n l =
+        let x = b +. (n *. incr) in
+        if check x a then (x :: l)
+        else loop (n +. 1.0) (x :: l) in
+    loop 1.0 []
+
 let main =
     Printf.printf "%d\n" @@ Array.length arr;
     Array.iter (fun x -> print_int @@ Array.length x) mat;
-    print_newline ()
+    print_newline ();
+    print_string "\nfrange 0.0 10.5 0.5\n";
+    List.map (Printf.printf "%.2f ") (frange 0.0 10.5 0.5);
+    print_string "\n\n"
 
 let () = main
