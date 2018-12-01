e313a523a02897ec3eda7195f498249a69791849
albertgoncalves <goncalves.albert@gmail.com>
2018-12-01 18:54:07 -0500

    Small improvements.

--- margin.ml
+++ margin.ml
@@ -1,32 +1,33 @@
+let init_surface ~w ~h =
+    let surface = Cairo.Image.create Cairo.Image.ARGB32 w h in
+    let cr = Cairo.create surface in
+    surface, cr
+
+let init_margins cr ~w ~h ~margin ~pad =
+    let pad_bound x = x *. (1.0 -. pad) in
+    Cairo.translate cr margin margin;
+    Cairo.scale cr (pad_bound w) (pad_bound h)
+
 let draw_rect cr ~x ~y ~w ~h ~r ~g ~b =
     Cairo.rectangle cr ~x:x ~y:y ~w:w ~h:h;
     Cairo.set_source_rgb cr r g b;
     Cairo.fill cr
 
-let init_margins cr margin bound =
-    Cairo.translate cr margin margin;
-    Cairo.scale cr bound bound
+let export surface filename =
+    Cairo.PNG.write surface filename;
+    Cairo.Surface.finish surface
 
 let () =
-    let bound = 120 in
-    let boundf = float_of_int bound in
-    let margin = boundf /. 10.0 in
-    let inner_boundf = boundf *. 0.8 in
-    let surface = Cairo.Image.create Cairo.Image.ARGB32 bound bound in
-    let cr = Cairo.create surface in
+    let bound = 200 in
+    let surface, cr = init_surface bound bound in
 
-    draw_rect cr
-        ~x:0.0 ~y:0.0 ~w:boundf ~h:boundf
-        ~r:0.0 ~g:0.0 ~b:0.0;
+    let bound = float_of_int bound in
+    draw_rect cr ~x:0.0 ~y:0.0 ~w:bound ~h:bound ~r:0.0 ~g:0.0 ~b:0.0;
+    init_margins cr ~w:bound ~h:bound ~margin:(bound /. 10.0) ~pad:0.2;
+    List.iter
+        (fun ((x, y, w, h), (r, g, b)) -> draw_rect cr x y w h r g b)
+        [ ((0.0, 0.0, 1.0, 1.0), (1.0, 1.0, 1.0))
+        ; ((0.25, 0.25, 0.5, 0.5), (0.2, 0.65, 0.55))
+        ];
 
-    init_margins cr margin inner_boundf;
-
-    draw_rect cr
-        ~x:0.0 ~y:0.0 ~w:1.0 ~h:1.0
-        ~r:1.0 ~g:1.0 ~b:1.0;
-    draw_rect cr
-        ~x:0.25 ~y:0.25 ~w:0.5 ~h:0.5
-        ~r:0.5 ~g:0.5 ~b:1.0;
-
-    Cairo.PNG.write surface "margin.png";
-    Cairo.Surface.finish surface
+    export surface "margin.png"
