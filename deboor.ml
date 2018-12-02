ae39098b0bef39fb5a68f01d6723896d63c01a3c
albertgoncalves <goncalves.albert@gmail.com>
2018-12-02 01:41:43 -0500

    More cleaning.

--- deboor.ml
+++ deboor.ml
@@ -3,63 +3,59 @@ let rec last default = function
     | [x] -> x
     | (_ :: xs) -> last default xs
 
-let rec zip_with f xs ys = match (xs, ys) with
+let rec zip_with f xs ys = match xs, ys with
     | (x :: xs), (y :: ys) -> f x y :: zip_with f xs ys
-    | ([], _) | (_, []) -> []
+    | [], _ | _, [] -> []
 
 let rec cox_deboor knots u k d =
-    if d == 0 then
-        if (float_of_int knots.(k) <= u) &&
-           (u < float_of_int knots.(k + 1)) then 1.0
-        else 0.0
+    if d == 0 then if knots.(k) <= u && u < knots.(k + 1) then 1.0 else 0.0
     else
-        let den1 = float_of_int @@ knots.(k + d) - knots.(k) in
-        let eq1 =
-            if den1 > 0.0 then
-                ((u -. float_of_int knots.(k)) /. den1) *.
-                (cox_deboor knots u k (d - 1))
-            else 0.0 in
-        let den2 = float_of_int @@ knots.(k + d + 1) - knots.(k + 1) in
-        let eq2 =
-            if den2 > 0.0 then
-                ((float_of_int knots.(k + d + 1) -. u) /. den2) *.
-                (cox_deboor knots u (k + 1) (d - 1))
-            else 0.0 in
-        eq1 +. eq2
+        let safe_div denom eq = if denom > 0.0 then eq else 0.0 in
+        let a =
+            let denom = knots.(k + d) -. knots.(k) in
+            let eq =
+                ((u -. knots.(k)) /. denom)
+                *. (cox_deboor knots u k (d - 1)) in
+            safe_div denom eq in
+        let b =
+            let denom = knots.(k + d + 1) -. knots.(k + 1) in
+            let eq =
+                ((knots.(k + d + 1) -. u) /. denom)
+                *. (cox_deboor knots u (k + 1) (d - 1)) in
+            safe_div denom eq in
+        a +. b
 
-let bspline cv n d =
-    let dim = List.length @@ List.hd cv in
-    let count = List.length cv in
-    let ul =
+let bspline cvs n d =
+    let dim = List.length @@ List.hd cvs in
+    let count = List.length cvs in
+    let us =
         let f x =
-            (x /. (float_of_int n -. 1.0)) *.
-            (float_of_int count -. float_of_int d) in
-        List.map f @@ List.init n float_of_int in
-    let knots =
-        Array.concat [ Array.make d 0
-                     ; Array.init (count - d + 1) (fun x -> x)
-                     ; Array.make d (count - d)
-                     ] in
-    let sample u =
-        if u = float_of_int @@ count - d then last [] cv
-        else
-            let rec loop k acc = function
-                | [] -> acc
-                | (c :: cv) ->
-                    let cox_deboor = cox_deboor knots u k d in
-                    let acc =
-                        let iter = (List.map (fun c -> cox_deboor *. c) c) in
-                        zip_with (+.) acc iter in
-                    loop (k + 1) acc cv in
-            loop 0 (List.init dim (fun _ -> 0.0)) cv in
-    List.map sample ul
+            (float_of_int x /. (float_of_int n -. 1.0))
+            *. (float_of_int count -. float_of_int d) in
+        List.init n f in
+    let knots = Array.concat [ Array.make d 0.0
+                             ; Array.init (count - d + 1) float_of_int
+                             ; Array.make d @@ float_of_int @@ count - d
+                             ] in
+    let rec loop u k accu = function
+        | [] -> accu
+        | (cv :: cvs) ->
+            let cox_deboor = cox_deboor knots u k d in
+            let accu =
+                let iter = List.map (fun x -> cox_deboor *. x) cv in
+                zip_with (+.) accu iter in
+            loop u (k + 1) accu cvs in
+    let zeros = List.init dim (fun _ -> 0.0) in
+    let thresh = float_of_int @@ count - d in
+    let sample u = if u = thresh then last [] cvs else loop u 0 zeros cvs in
+    List.map sample us
 
 let main () =
-    let cv = [ [ 50.0; 25.0;   0.0 ]
-             ; [ 59.0; 12.0; (-1.0)]
-             ; [ 50.0; 10.0;   1.0 ]
-             ; [ 57.0;  2.0;   2.0 ]
-             ; [ 40.0;  4.0; (-1.0)]
-             ; [ 40.0; 14.0;   0.0 ]
-             ] in
-    bspline cv 10 3
+    let cvs = [ [ 50.0; 25.0;   0.0 ]
+              ; [ 59.0; 12.0; (-1.0)]
+              ; [ 50.0; 10.0;   1.0 ]
+              ; [ 57.0;  2.0;   2.0 ]
+              ; [ 40.0;  4.0; (-1.0)]
+              ; [ 40.0; 14.0;   0.0 ]
+              ] in
+    bspline cvs 10 3
