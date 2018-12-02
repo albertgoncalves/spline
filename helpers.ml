78ea69fdc00e74a5293f566df3d3ecb2b350aed2
albertgoncalves <goncalves.albert@gmail.com>
2018-12-02 10:57:37 -0500

    Forgot this.

---
+++ helpers.ml
@@ -0,0 +1,8 @@
+let rec last default = function
+    | [] -> default
+    | [x] -> x
+    | (_ :: xs) -> last default xs
+
+let rec zip_with f xs ys = match xs, ys with
+    | (x :: xs), (y :: ys) -> f x y :: zip_with f xs ys
+    | [], _ | _, [] -> []
