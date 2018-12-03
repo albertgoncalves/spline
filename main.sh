4243db40f97188d34ad567e5cf809554a0526104
albertgoncalves <goncalves.albert@gmail.com>
2018-12-02 19:49:11 -0500

    Words...

--- main.sh
+++ main.sh
@@ -2,6 +2,10 @@
 
 cd src
 
+for f in generators helpers drawing deboor; do
+    ocamlfind ocamlc -c $f.ml -package cairo2
+done
+
 ocamlfind ocamlopt \
     -package cairo2 generators.ml helpers.ml drawing.ml deboor.ml \
     -linkpkg $1.ml -o $1
