cbcf1a1e7dc19b6810f71e68f9112eb39e1ef669
albertgoncalves <goncalves.albert@gmail.com>
2018-12-08 09:24:02 -0500

    Nothing to see here.

--- src/sentence.ml
+++ src/sentence.ml
@@ -5,6 +5,9 @@ module L = List
 module R = Random
 module S = Deboor
 
+let fl_of_in = float_of_int
+let in_of_fl = int_of_float
+
 let () =
     R.self_init ();
 
@@ -16,16 +19,16 @@ let () =
             ~y_min:0.0 ~y_max:1.0 in
 
     let pad = 0.25 in
-    let k = float_of_int @@ H.sum_int ns in
+    let k = fl_of_in @@ H.sum_int ns in
     let j = 1.0 +. (pad /. 2.0) +. (k -. (k *. pad)) in
-    let res = 200 * (int_of_float k + 1) in
+    let res = 200 * (in_of_fl k + 1) in
     let y_bound = 350 in
-    let x_bound = int_of_float @@ ceil @@ float_of_int y_bound *. j in
+    let x_bound = in_of_fl @@ ceil @@ fl_of_in y_bound *. j in
 
     let surface, cr = D.init_surface x_bound y_bound in
     D.antialias cr;
 
-    let x_bound = float_of_int x_bound and y_bound = float_of_int y_bound in
+    let x_bound = fl_of_in x_bound and y_bound = fl_of_in y_bound in
     D.rect cr ~x:0.0 ~y:0.0 ~w:x_bound ~h:y_bound ~r:1.0 ~g:1.0 ~b:1.0;
 
     D.margins cr ~w:y_bound ~h:y_bound ~pad;
@@ -33,6 +36,7 @@ let () =
     let splines = List.map (fun word -> S.bspline word res 3) words in
 
     let r = 0.0 and g = 0.0 and b = 0.0 in
-    List.iter (fun spline -> D.lines cr ~pts:spline ~lw:0.005 ~r ~g ~b) splines;
+    List.iter
+        (fun spline -> D.lines cr ~pts:spline ~lw:0.005 ~r ~g ~b) splines;
 
     D.export surface "../out/sentence.png"
