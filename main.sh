34f50fdb196fedf95c3dc2710f4a6258ad102ed4
albertgoncalves <goncalves.albert@gmail.com>
2019-02-05 10:25:20 -0500

    The right way to do this.

--- main.sh
+++ main.sh
@@ -4,13 +4,9 @@ set -e
 
 cd src
 
-for f in generators helpers drawing deboor; do
-    ocamlfind ocamlopt -c $f.ml -package cairo2
-done
-
 ocamlfind ocamlopt \
-    -package cairo2 generators.ml helpers.ml drawing.ml deboor.ml \
-    -linkpkg $1.ml -o $1
+    -package cairo2 \
+    -linkpkg generators.ml helpers.ml drawing.ml deboor.ml $1.ml -o $1
 ./$1
 
 output=../out/$1.png
